483386207fbceab1fda83342a39669ef6429cbb4
artemkosterin <160392965+artemkosterin@users.noreply.github.com>
2024-03-19 18:58:57 +0300

    b117 Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int count = 0;
+  int counter = 0;
   for (int i = 0; i < size; i++) {
     if (arr[i] == value) {
-      count++;
+      counter++;
     }
   }
-  return count;
+  return counter;
   return 0;
 }
